66698f841c73ee624e487ae8d3af9aa6d002206b
JustinKringler1 <91436313+JustinKringler1@users.noreply.github.com>
2024-11-02 21:17:21 -0500

    Update best_value_set.py

--- code/analytics/best_value_set.py
+++ code/analytics/best_value_set.py
@@ -36,17 +36,26 @@ result_df = result_df.rename(columns={"source": "set"})
 # Remove the dollar sign and convert 'Market Price' to numeric
 result_df["Market Price"] = result_df["Market Price"].replace('[\$,]', '', regex=True).astype(float)
 
+result_df["Market Price"] = pd.to_numeric(result_df["Market Price"], errors='coerce')
+
+
 # Group by 'Rarity' and 'set', and calculate the mean for 'Market Price'
 grouped_df = result_df.groupby(["set", "Rarity"])["Market Price"].mean().reset_index()
 
+print(grouped_df.head())
+
 # Load the pull rates data
 pull_rates_df = pd.read_csv("data/pull_rates.csv")
+
+print(pull_rates_df.head())
 
 # Ensure "Probability" is numeric, handling any conversion errors
 pull_rates_df["Probability"] = pd.to_numeric(pull_rates_df["Probability"], errors='coerce')
 
 # Perform the inner join on 'set' column
-merged_df = pd.merge(grouped_df, pull_rates_df, on="set", how="inner")
+merged_df = pd.merge(grouped_df, pull_rates_df, on="set", how="left")
+
+print(merged_df.head())
 
 # Calculate the 'value' column by multiplying 'Probability' with 'Market Price'
 merged_df["value"] = merged_df["Probability"] * merged_df["Market Price"]
